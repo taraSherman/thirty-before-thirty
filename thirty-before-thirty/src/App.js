11253035bfebf918079282bfe292afc7d88a9a59
Kadeem140 <Beckford.kadeem@yahoo.com>
2019-11-13 22:16:20 -0500

    Created basic form skeleton

---
+++ thirty-before-thirty/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+import { Formik, Form , Field } from 'formik';
+
+function App() {
+  return (
+    <Formik 
+        initialValues= {{ username: '', password: ''}}
+        onSubmit={(...appUsers) => {
+            console.log(appUsers)
+          
+        }}
+        render = {props => {
+            return(
+              <Form>
+                <Field type='text' name='username' placeholder='Username'/>
+                <Field type='password' name='password' placeholder='Password'/>
+
+                <input type='submit' />
+
+              </Form>
+            )
+
+        }}
+
+    />
+  )
+}
+
+export default App;
