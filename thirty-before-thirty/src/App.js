78adabe64e6563a388aebe9a414260f809ee13ee
Anna Morris <annapatton444@gmail.com>
2019-11-16 18:56:59 -0600

    more styleing

--- thirty-before-thirty/src/App.js
+++ thirty-before-thirty/src/App.js
@@ -2,16 +2,20 @@ import React from "react";
 import Login from "./Component/Login";
 import styled from "styled-components";
 
-const wrapperDiv = styled.div`
+const WrapperDiv = styled.div`
   font-family: sans-sarif;
   text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
 
 function App() {
   return (
-    <div>
-      This is an app!<Login></Login>
-    </div>
+    <WrapperDiv>
+      <Login></Login>
+    </WrapperDiv>
   );
 }
 
