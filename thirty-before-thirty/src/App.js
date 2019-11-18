200390eeed8ace27d4061bef437bef3c7ab47114
Anna Morris <annapatton444@gmail.com>
2019-11-17 19:40:56 -0600

    break header in to own component

--- thirty-before-thirty/src/App.js
+++ thirty-before-thirty/src/App.js
@@ -1,20 +1,29 @@
 import React from "react";
 import Login from "./Component/Login";
+import Header from "./Component/Header"
 import styled from "styled-components";
 
 const WrapperDiv = styled.div`
   font-family: sans-sarif;
-  text-align: center;
+  
+`;
+
+const MainDiv = styled.div`text-align: center;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  background-color: #F7F9FB;
 `;
 
 function App() {
   return (
     <WrapperDiv>
-      <Login></Login>
+      <Header></Header>
+      <MainDiv>
+        {/* Routes Here */}
+        <Login></Login>
+      </MainDiv>
     </WrapperDiv>
   );
 }
