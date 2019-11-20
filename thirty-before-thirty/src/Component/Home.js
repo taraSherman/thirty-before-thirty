b14af8ef48adf4a0f27c87877f57001966c5591f
Tara Sherman <taradelin@gmail.com>
2019-11-19 23:45:47 -0500

    Changed itemListView to ItemList in Home.js

--- thirty-before-thirty/src/Component/Home.js
+++ thirty-before-thirty/src/Component/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
-import { itemListView } from './views'
+import ItemList from '../Component/ItemList';
 
 function Home() {
   return (
     <Provider store={store}>
       <div className="App">
-        <itemListView />
+        <ItemList />
       </div>
     </Provider>
   );
